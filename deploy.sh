96365786bb758772b46a96b665d0eecf3cfaabbe
Joshua Drewlow <32765631+drewjosh@users.noreply.github.com>
2022-03-14 13:34:42 +0100

    updates to deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,19 +6,21 @@ set -e
 # build
 npm run build
 
-# navigate into the build output directory
-cd dist
+# move generated files out of spa folder as children of dist
+cp -v dist/spa/* dist
+rm -r dist/spa
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
+cd dist
 git init
-git granch -m main
+git branch -m main
 git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:drewjosh/midata-quasar-starter-app.github.io.git main
+git push -f git@github.com:drewjosh/midata-quasar-starter-app.git gh-pages
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
