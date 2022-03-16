62de51a2f7f8d6e914301d70ae6b29da03cb43b9
Joshua Drewlow <32765631+drewjosh@users.noreply.github.com>
2022-03-16 09:10:22 +0100

    comment for public path setting

--- quasar.conf.js
+++ quasar.conf.js
@@ -53,7 +53,7 @@ module.exports = configure(function (ctx) {
 
       // transpile: false,
       publicPath: process.env.NODE_ENV === 'production'
-        ? '/midata-quasar-starter-app/'
+        ? '/midata-quasar-starter-app/' // necessary if you serve app on GitHub pages, corresponds to repository name
         : '/',
 
       // Add dependencies for transpiling with Babel (Array of string/regex)
