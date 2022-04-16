a61c2bbaf104b165c6c873797c973f2f63a8fff2
8O0 <lukas.loosli@hotmail.de>
2022-04-16 20:26:16 +0200

    added the epd to the login

--- src/components/LoginCard.vue
+++ src/components/LoginCard.vue
@@ -19,17 +19,48 @@
         >
       </q-card-actions>
     </q-card>
+    <q-card class="card">
+      <q-card-section>
+        <div class="text-h4 text-weight-thin">EPD Login</div>
+        <div class="text-body1">
+          Damit sie EPD verwenden können, müssen Sie sich zuerst einloggen.
+        </div>
+
+      </q-card-section>
+      <q-separator inset />
+      <q-card-actions>
+        <q-btn
+          @click="connectEPD"
+          :disabled="connectedEpd == true"
+          flat
+          class="epd-fade full-width"
+          color="white"
+        > {{labelEPD}}</q-btn>
+      </q-card-actions>
+    </q-card>
   </div>
 </template>
 
 <script lang="ts">
+import { connected } from 'process';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
+  data: function () {
+    return {
+      connectedEpd: false, labelEPD: 'Mit dem EPD verbinden',
+    }
+  },
   name: 'LoginCard',
+
   methods: {
     connect() {
       this.$midata.authenticate();
+    },
+    connectEPD: function() {
+      this.connectedEpd = true;
+      this.labelEPD = 'Sie sind mit EPD verbunden';
+      return this.labelEPD;
     },
   },
 });
@@ -37,10 +68,10 @@ export default defineComponent({
 
 <style lang="sass" scoped>
 .card
-    max-width: 500px
+  max-width: 500px
 
 .container
-    display: flex
-    justify-content: center
-    margin-top: 50px
+  display: flex
+  justify-content: center
+  margin-top: 50px
 </style>
