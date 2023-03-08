4408f03c3e7253f87bd06f6c1cbd83b5413b3cc6
Nicolas Gujer <nicolas.gujer@protonmail.com>
2023-03-08 16:08:06 +0100

    quasar virtual-scroll issue fixed --> The quasar virtual scroll had an issue while scrolling. It jumped to the top constantly when the items array was bigger than 40 elements. That is fixed now by wrapping all the elements inside the v-slot into a div. Thanks heg2 for the fix idea.

--- src/components/midata/BodyTemperature/MidataBodyTemperature.vue
+++ src/components/midata/BodyTemperature/MidataBodyTemperature.vue
@@ -13,86 +13,88 @@
         style="max-height: 300px"
       >
         <template v-slot="{ item, index }">
-          <q-item clickable dense v-ripple :key="index">
-            <q-item-section avatar>
-              <q-avatar
-                icon="thermostat"
-                text-color="white"
-                class="midata-fade"
+          <div>
+            <q-item clickable dense v-ripple :key="index">
+              <q-item-section avatar>
+                <q-avatar
+                  icon="thermostat"
+                  text-color="white"
+                  class="midata-fade"
+                >
+                </q-avatar>
+              </q-item-section>
+              <q-item-section>
+                <q-item-label
+                  lines="1"
+                >
+                  Körpertemperatur
+                </q-item-label>
+                <q-item-label caption>
+                  Wert: {{ item.valueQuantity.value }}
+                  {{ item.valueQuantity.unit }} ({{
+                    item.bodySite.coding[0].display
+                  }})
+                </q-item-label>
+                <q-item-label caption>
+                  Datum:
+                  {{ store.formatDate(item.issued) }} ({{ store.fromNow(item.issued) }})
+                </q-item-label>
+              </q-item-section>
+              <q-btn
+                color="primary"
+                outlined
+                flat
+                @mouseover="store.setCurrentObservation(item.id)"
+                @click.stop="showEditDialog = true"
+                icon="edit"
+                class="gt-xs"
               >
-              </q-avatar>
-            </q-item-section>
-            <q-item-section>
-              <q-item-label
-                lines="1"
+                Bearbeiten
+              </q-btn>
+              <q-btn
+                color="red"
+                outlined
+                flat
+                @mouseover="store.setCurrentObservation(item.id)"
+                @click.stop="showDeleteDialog = true"
+                icon="delete"
+                class="gt-xs"
               >
-                Körpertemperatur
-              </q-item-label>
-              <q-item-label caption>
-                Wert: {{ item.valueQuantity.value }}
-                {{ item.valueQuantity.unit }} ({{
-                  item.bodySite.coding[0].display
-                }})
-              </q-item-label>
-              <q-item-label caption>
-                Datum:
-                {{ store.formatDate(item.issued) }} ({{ store.fromNow(item.issued) }})
-              </q-item-label>
-            </q-item-section>
-            <q-btn
-              color="primary"
-              outlined
-              flat
-              @mouseover="store.setCurrentObservation(item.id)"
-              @click.stop="showEditDialog = true"
-              icon="edit"
-              class="gt-xs"
-            >
-              Bearbeiten
-            </q-btn>
-            <q-btn
-              color="red"
-              outlined
-              flat
-              @mouseover="store.setCurrentObservation(item.id)"
-              @click.stop="showDeleteDialog = true"
-              icon="delete"
-              class="gt-xs"
-            >
-              Löschen
-            </q-btn>
-            <q-btn
-              color="primary"
-              outlined
-              round
-              flat
-              @mouseover="store.setCurrentObservation(item.id)"
-              @click.stop="showEditDialog = true"
-              icon="edit"
-              class="lt-sm"
-            >
-            </q-btn>
-            <q-btn
-              color="red"
-              outlined
-              round
-              flat
-              @mouseover="store.setCurrentObservation(item.id)"
-              @click.stop="showDeleteDialog = true"
-              icon="delete"
-              class="lt-sm"
-            >
-            </q-btn>
-          </q-item>
-          <q-item v-if='store.observationsExpanded' :key='index + "_codeblock"' >
-            <q-item-section clickable @click='store.copyToClipBoard(item, "Observation Resource")'>
-              <highlightjs
-                lang="json"
-                :code="JSON.stringify(item, null, 2)"
-              ></highlightjs>
-            </q-item-section>
-          </q-item>
-          <q-separator inset spaced />
+                Löschen
+              </q-btn>
+              <q-btn
+                color="primary"
+                outlined
+                round
+                flat
+                @mouseover="store.setCurrentObservation(item.id)"
+                @click.stop="showEditDialog = true"
+                icon="edit"
+                class="lt-sm"
+              >
+              </q-btn>
+              <q-btn
+                color="red"
+                outlined
+                round
+                flat
+                @mouseover="store.setCurrentObservation(item.id)"
+                @click.stop="showDeleteDialog = true"
+                icon="delete"
+                class="lt-sm"
+              >
+              </q-btn>
+            </q-item>
+            <q-item v-if='store.observationsExpanded' :key='index + "_codeblock"' >
+              <q-item-section clickable @click='store.copyToClipBoard(item, "Observation Resource")'>
+                <highlightjs
+                  lang="json"
+                  :code="JSON.stringify(item, null, 2)"
+                ></highlightjs>
+              </q-item-section>
+            </q-item>
+            <q-separator inset spaced />
+          </div>
         </template>
       </q-virtual-scroll>
     </q-card-section>
